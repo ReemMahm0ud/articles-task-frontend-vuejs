8dd89fe705f364e3379d4d5a09c314c7b36fd5e1
ReemMahm0ud <reemhashad82@gmail.com>
2021-12-07 00:37:38 +0200

    add routes

--- articles-app/src/router/index.js
+++ articles-app/src/router/index.js
@@ -1,21 +1,32 @@
+//import Vue from 'vue'
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
+import ArticleCreate from "../views/ArticleCreate.vue";
+import ArticleList from "../views/ArticleList.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "article-list",
+    component: ArticleList,
   },
+//   {
+//     path: "/article/:id",
+//     name: "article-show",
+//     component:ArticleShow,
+//     props:true
+//   },
+//   {
+//       path:"/articleEdit/:id",
+//       name:"article-edit",
+//       component:ArticleEdit,
+//       props:true
+//   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+      path:"/article/create",
+      name: "article-create",
+      component:ArticleCreate
+  }
 ];
 
 const router = createRouter({
