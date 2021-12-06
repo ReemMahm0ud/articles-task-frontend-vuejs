82dcf75f83b15936f9e651be136f85dbfb0fb728
ReemMahm0ud <reemhashad82@gmail.com>
2021-12-07 00:37:19 +0200

    add service data layer

--- articles-app/src/services/ArticleService.js
+++ articles-app/src/services/ArticleService.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://127.0.0.1:8000/api',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
     }
 })
 
