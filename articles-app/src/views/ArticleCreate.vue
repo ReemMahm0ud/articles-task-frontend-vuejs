bc08960317fa9de6223fe3f0b5a750d5683b1bd6
ReemMahm0ud <reemhashad82@gmail.com>
2021-12-07 11:40:42 +0200

    add article create view

---
+++ articles-app/src/views/ArticleCreate.vue
@@ -0,0 +1,15 @@
+<template>
+  <article-form />
+</template>
+
+<script>
+// @ is an alias to /src
+import ArticleForm from '../components/ArticleForm.vue';
+
+export default {
+  name: "Article-create",
+  components: {
+    ArticleForm,
+  },
+};
+</script>
